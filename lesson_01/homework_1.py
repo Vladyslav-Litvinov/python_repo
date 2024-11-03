c53ef1f5d694f02feddbf20b1d290d11ea672cfd
Litvinov Vladyslav <vladyslav.litvinov@zimad.com>
2024-11-03 23:09:03 +0200

    Виконані зміни

--- lesson_01/homework_1.py
+++ lesson_01/homework_1.py
@@ -1,63 +1,55 @@
-# task 01 == Виправте синтаксичні помилки
-print("Hello", end = " ")
-    print("world!")
+# task 01
+print("Hello", end=" ")
+print("world!")
 
-# task 02 == Виправте синтаксичні помилки
+# task 02
 hello = "Hello"
 world = "world"
 if True:
-print(f"{hello} {world}!")
+    print(f"{hello} {world}!")
 
-# task 03  == Вcтавте пропущену змінну у ф-цію print
+# task 03
 for letter in "Hello world!":
-    print()
+    print(letter)
 
-# task 04 == Зробіть так, щоб кількість бананів була
-# завжди в чотири рази більша, ніж яблук
+# task 04
 apples = 2
-banana = x
+bananas = apples * 4
 
-# task 05 == виправте назви змінних
-1_storona = 1
-?torona_2 = 2
-сторона_3 = 3
-$torona_4 = 4
+# task 05
+side_1 = 1
+side_2  = 2
+side_3  = 3
+side_4  = 4
 
-# task 06 == Порахуйте периметр фігури з task 05
-# та виведіть його для користувача
-perimetery = ? + ? + ? + ?
-print()
+# task 06
+perimeter = side_1 + side_2 + side_3 + side_4
+print("Периметр фігури:", perimeter)
 
-
-"""
-    # Задачі 07 -10:
-    # Переведіть задачі з книги "Математика, 2 клас"
-    # на мову пітон і виведіть відповідь, так, щоб було
-    # зрозуміло дитині, що навчається в другому класі
-"""
 # task 07
-"""
-У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
-Скільки всього дерев посадили в саду?
-"""
+apple_trees = 4
+pear_trees = apple_trees + 5
+plum_trees = apple_trees - 2
+total_trees = apple_trees + pear_trees + plum_trees
+print("Всього дерев у саду:", total_trees)
 
 # task 08
-"""
-До обіда температура повітря була на 5 градусів вище нуля.
-Після обіду температура опустилася на 10 градусів.
-Надвечір потепліло на 4 градуси. Яка температура надвечір?
-"""
+morning_temp = 5
+afternoon_temp = morning_temp - 10
+evening_temp = afternoon_temp + 4
+print("Температура надвечір:", evening_temp, "градусів")
 
 # task 09
-"""
-Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
-1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
-Скількі сьогодні дітей у театральному гуртку?
-"""
+boys = 24
+girls = boys // 2
+boys_now = boys - 1
+girls_now = girls - 2
+total_children = boys_now + girls_now
+print("Сьогодні дітей у театральному гуртку:", total_children)
 
 # task 10
-"""
-Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
-а третя - як половина вартості першої та другої разом.
-Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
-"""
+first_book_price = 8
+second_book_price = first_book_price + 2
+third_book_price = (first_book_price + second_book_price) / 2
+total_cost = first_book_price + second_book_price + third_book_price
+print("Вартість усіх книг:", total_cost, "грн")
