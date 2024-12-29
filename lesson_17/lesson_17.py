8ddf13fc50f83b23c2c2ca1b9213890a6e0bc7ed
Litvinov Vladyslav <vladyslav.litvinov@zimad.com>
2024-12-29 10:44:40 +0200

    Домашка 17 version 3

--- lesson_17/lesson_17.py
+++ lesson_17/lesson_17.py
@@ -6,28 +6,26 @@ class Rhombus:
     def __setattr__(self, name, value):
         if name == "side_a":
             if value <= 0:
-                raise ValueError("Значение стороны должно быть больше 0.")
+                raise ValueError("Значення сторони повинно бути більше 0.")
         if name in ["angle_a", "angle_b"]:
             if not (0 < value < 180):
-                raise ValueError("Угол должен быть между 0 и 180 градусами.")
-            # Проверяем сумму углов при установке одного из углов
+                raise ValueError("Кут повинен бути між 0 та 180 градусів.")
+            # Перевіряємо суму кутів при встановленні одного з кутів
             other_angle = 180 - value if name == "angle_a" else 180 - value
             if name == "angle_a" and hasattr(self, "angle_b") and other_angle != self.angle_b:
-                raise ValueError("Сумма углов должна быть равна 180 градусам.")
+                raise ValueError("Сума кутів повинна бути рівна 180 градусів.")
             if name == "angle_b" and hasattr(self, "angle_a") and other_angle != self.angle_a:
-                raise ValueError("Сумма углов должна быть равна 180 градусам.")
+                raise ValueError("Сума кутів повинна бути рівна 180 градусів.")
             super().__setattr__("angle_b" if name == "angle_a" else "angle_a", other_angle)
 
         super().__setattr__(name, value)
 
     def __str__(self):
-        return f"Ромб со стороной {self.side_a} и углами {self.angle_a}° и {self.angle_b}°."
-
-
-# Пример использования класса:
+        return f"Ромб зі стороною {self.side_a} і кутами {self.angle_a}° та {self.angle_b}°."
+# Приклад використання класу:
 try:
-    rhombus = Rhombus(4, 80)
+    rhombus = Rhombus(4, 66)
     print(rhombus)
-    rhombus.angle_b = 100  # Это вызовет ошибку
+    rhombus.angle_b = 50  # Це викличе помилку
 except ValueError as e:
     print(e)
